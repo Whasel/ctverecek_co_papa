9c487c9c60310adcf16833775ddbd5aba2f58be0
Václav Fišar <fisarv22@spstrutnov.cz>
2023-01-25 13:20:40 +0100

    zvětšování

--- papa.py
+++ papa.py
@@ -2,15 +2,17 @@ import random
 import pygame
 
 pygame.init()
-
+speed = 5
 OSA_X = 1024
 OSA_Y = 800
 BLACK = (0, 0, 0)
 GREY = (77,77,77)
 GOLD = (255,215,0)
-speed = 0.5
+FPS = 60
 main_cube = 50
 smaller_cube = 25
+
+hodiny = pygame.time.Clock()
 
 main_x = (OSA_X - main_cube) / 2
 main_y = (OSA_Y - main_cube) / 2
@@ -30,6 +32,7 @@ pygame.display.set_caption('čtvereček co papá')
 okno = pygame.display.set_mode((OSA_X, OSA_Y))
 
 while True:
+    hodiny.tick(FPS)
     for udalost in pygame.event.get():
         if udalost.type == pygame.QUIT:
             pygame.quit()
@@ -48,10 +51,7 @@ while True:
         main_y += speed
     if keys[pygame.K_UP]:
         main_y -= speed
-    if keys[pygame.K_SPACE] == True:
-        speed = 2
-    if keys[pygame.K_SPACE] == False:
-        speed = 0.5
+
     if main_x > OSA_X - main_cube:
         main_x = OSA_X - main_cube
     if main_y > OSA_Y - main_cube:
@@ -60,7 +60,32 @@ while True:
         main_x = 0
     if main_y < 0:
         main_y = 0  
-        
+    if main_x == sc_x:
+        main_cube += 10
+        speed /= 1.1
+    elif main_y == sc_y:
+        main_cube += 10
+        speed /= 1.1
+    if main_x == sc2_x:
+        main_cube += 10
+        speed /= 1.1
+    elif main_y == sc2_y:
+        main_cube += 10
+        speed /= 1.1
+    if main_x == sc3_x:
+        main_cube += 100
+        speed /= 1.1
+    elif main_y == sc3_y:
+        main_cube += 10
+        speed /= 1.1
+    if main_x == sc4_x:
+        main_cube += 10
+        speed /= 1.1
+    elif main_y == sc4_y:
+        main_cube += 10
+        speed /= 1.1
+    
+    
 
         
         
