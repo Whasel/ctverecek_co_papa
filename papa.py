757428da77b523e6bf7245e536052b69f0c18a36
Václav Fišar <fisarv22@spstrutnov.cz>
2023-01-30 18:17:49 +0100

    finished
    kostička co papa finished :)

--- papa.py
+++ papa.py
@@ -1,30 +1,28 @@
 import random
 import pygame
+import sys
 
 pygame.init()
 speed = 5
-OSA_X = 1024
-OSA_Y = 800
+OSA_X = 1920
+OSA_Y = 1080
 BLACK = (0, 0, 0)
 GREY = (77,77,77)
 GOLD = (255,215,0)
 FPS = 60
 main_cube = 50
 smaller_cube = 25
-
+big = 5
+decrease_speed = 0.25
 hodiny = pygame.time.Clock()
+mc = pygame.Rect(512, 400, main_cube, main_cube) 
 
-main_x = (OSA_X - main_cube) / 2
-main_y = (OSA_Y - main_cube) / 2
-sc_x = (OSA_X - smaller_cube) - random.randint(1,OSA_X - smaller_cube)
-sc_y = (OSA_Y - smaller_cube) - random.randint(1,OSA_Y - smaller_cube)
-sc2_x = (OSA_X - smaller_cube) - random.randint(1,OSA_X - smaller_cube)
-sc2_y = (OSA_Y - smaller_cube) - random.randint(1,OSA_Y - smaller_cube)
-sc3_x = (OSA_X - smaller_cube) - random.randint(1,OSA_X - smaller_cube)
-sc3_y = (OSA_Y - smaller_cube) - random.randint(1,OSA_Y - smaller_cube)
-sc4_x = (OSA_X - smaller_cube) - random.randint(1,OSA_X - smaller_cube)
-sc4_y = (OSA_Y - smaller_cube) - random.randint(1,OSA_Y - smaller_cube)
+cubes = []
+for i in range(15):
+    rect = pygame.Rect(random.randint(25,1870), random.randint(25,1030), smaller_cube, smaller_cube)
+    cubes.append(rect)
 
+    
 
 okno = pygame.display.set_mode((OSA_X, OSA_Y))
 pygame.display.set_caption('čtvereček co papá')
@@ -44,55 +42,33 @@ while True:
         pygame.quit()
         sys.exit()
     if keys[pygame.K_RIGHT]:
-        main_x += speed
+        mc.x += speed
     if keys[pygame.K_LEFT]:
-        main_x -= speed
+        mc.x -= speed
     if keys[pygame.K_DOWN]:
-        main_y += speed
+        mc.y += speed
     if keys[pygame.K_UP]:
-        main_y -= speed
+        mc.y -= speed
 
-    if main_x > OSA_X - main_cube:
-        main_x = OSA_X - main_cube
-    if main_y > OSA_Y - main_cube:
-        main_y = OSA_Y - main_cube
-    if main_x < 0:
-        main_x = 0
-    if main_y < 0:
-        main_y = 0  
-    if main_x == sc_x:
-        main_cube += 10
-        speed /= 1.1
-    elif main_y == sc_y:
-        main_cube += 10
-        speed /= 1.1
-    if main_x == sc2_x:
-        main_cube += 10
-        speed /= 1.1
-    elif main_y == sc2_y:
-        main_cube += 10
-        speed /= 1.1
-    if main_x == sc3_x:
-        main_cube += 100
-        speed /= 1.1
-    elif main_y == sc3_y:
-        main_cube += 10
-        speed /= 1.1
-    if main_x == sc4_x:
-        main_cube += 10
-        speed /= 1.1
-    elif main_y == sc4_y:
-        main_cube += 10
-        speed /= 1.1
-    
-    
-
+    if mc.x > OSA_X - main_cube:
+        mc.x = OSA_X - main_cube
+    if mc.y > OSA_Y - main_cube:
+       mc.y = OSA_Y - main_cube
+    if mc.x < 0:
+        mc.x = 0
+    if mc.y < 0:
+        mc.y = 0
         
+    for i in cubes:
+        collide = pygame.Rect.colliderect(mc, i)
+        if collide == True:
+            cubes.remove(i)
+            mc.height += big
+            mc.width += big
+            speed -= decrease_speed
         
     okno.fill(GREY)
-    pygame.draw.rect(okno, GOLD, (sc_x, sc_y, smaller_cube, smaller_cube))
-    pygame.draw.rect(okno, GOLD, (sc2_x, sc2_y, smaller_cube, smaller_cube))
-    pygame.draw.rect(okno, GOLD, (sc3_x, sc3_y, smaller_cube, smaller_cube))
-    pygame.draw.rect(okno, GOLD, (sc4_x, sc4_y, smaller_cube, smaller_cube))
-    pygame.draw.rect(okno, BLACK, (main_x, main_y, main_cube, main_cube))  
+    for cube in cubes:
+       pygame.draw.rect(okno, GOLD, cube) 
+    pygame.draw.rect(okno, BLACK, mc) 
     pygame.display.update()
